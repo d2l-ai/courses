19e1c2fea5603210dedf351324e3eba2ab9a2034
mli <muli@cs.cmu.edu>
2018-12-19 00:55:04 +0000

    update

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ set -x
 BUCKET=s3://courses.diveintodeeplearning.org
 
 aws s3 sync --exclude '.git/*' --exclude '*.sh' --exclude 'README.md' \
-    --delete . $BUCKET --acl 'public-read'
+    . $BUCKET --acl 'public-read'
